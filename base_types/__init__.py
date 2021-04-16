702865bb2c914be5a0c8e1e7e98a132b2bdb8f79
Alex Volkov <alexv@fragmentprotocol.com>
2021-04-16 18:34:40 +0300

    feat: add base types

---
+++ base_types/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module contains all the basic types of objects.
+"""
+from base_types.block import Block
+from base_types.output import Output
+from base_types.scanner import Scanner
+from base_types.monitor import Monitor
+from base_types.network import Network
+from base_types.block_event import BlockEvent
+from base_types.transaction import Transaction
+from base_types.transaction_receipt import TransactionReceipt
